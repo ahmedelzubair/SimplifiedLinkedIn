205962ad14870195c5de1efe2788ed9038ef1f95
ahmedelzubair <ahmed.aau3@gmail.com>
2022-05-17 12:46:35 +0300

    init commit

---
+++ src/main/java/com/ahmedelzubair/simplifiedlinkedin/SimplifiedLinkedInApplication.java
@@ -0,0 +1,13 @@
+package com.ahmedelzubair.simplifiedlinkedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedLinkedInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplifiedLinkedInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmedelzubair/simplifiedlinkedin/SimplifiedLinkedInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmedelzubair.simplifiedlinkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedLinkedInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
