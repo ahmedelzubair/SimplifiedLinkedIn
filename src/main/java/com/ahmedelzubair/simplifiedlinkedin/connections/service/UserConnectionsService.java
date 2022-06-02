3e95eebb4380a493423f46b30115bbb88826b036
Ahmed M. Elzubair <ahmed.aau3@gmail.com>
2022-06-02 18:45:59 +0300

    UserConnectionsService added Closes #14

---
+++ src/main/java/com/ahmedelzubair/simplifiedlinkedin/connections/service/UserConnectionsService.java
@@ -0,0 +1,18 @@
+package com.ahmedelzubair.simplifiedlinkedin.connections.service;
+
+import com.ahmedelzubair.simplifiedlinkedin.connections.domain.dto.UserConnectionDTO;
+
+import java.util.Optional;
+
+public interface UserConnectionsService {
+
+    void createUserConnection(UserConnectionDTO userConnectionDTO);
+
+    UserConnectionDTO updateUserConnection(UserConnectionDTO userConnectionDTO);
+
+    void deleteUserConnection(UserConnectionDTO userConnectionDTO);
+
+    Optional<UserConnectionDTO> findUserConnectionById(Long userConnectionId);
+
+
+}
