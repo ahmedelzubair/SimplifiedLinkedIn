9dec8b390b0d94674e8d17e7329291cc3d49f982
Ahmed M. Elzubair <ahmed.aau3@gmail.com>
2022-06-02 18:44:11 +0300

    AddressService added Closes #13

--- src/main/java/com/ahmedelzubair/simplifiedlinkedin/address/service/AddressService.java
+++ src/main/java/com/ahmedelzubair/simplifiedlinkedin/address/service/AddressService.java
@@ -4,6 +4,7 @@ import com.ahmedelzubair.simplifiedlinkedin.address.domain.dto.AddressDTO;
 
 import java.util.Optional;
 
+// for closing GitHub issue
 public interface AddressService {
 
     void createAddress(AddressDTO addressDTO);
