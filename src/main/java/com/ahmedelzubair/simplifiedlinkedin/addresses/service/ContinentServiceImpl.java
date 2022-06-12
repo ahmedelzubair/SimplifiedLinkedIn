38aa9b6c2676c0c18043cb093af8b4b36639a57f
Ahmed M. Elzubair <ahmed.aau3@gmail.com>
2022-06-12 18:37:54 +0300

    ContinentServiceImpl done

--- src/main/java/com/ahmedelzubair/simplifiedlinkedin/addresses/service/ContinentServiceImpl.java
+++ src/main/java/com/ahmedelzubair/simplifiedlinkedin/addresses/service/ContinentServiceImpl.java
@@ -4,9 +4,12 @@ import com.ahmedelzubair.simplifiedlinkedin.addresses.domain.Continent;
 import com.ahmedelzubair.simplifiedlinkedin.addresses.domain.dto.ContinentDTO;
 import com.ahmedelzubair.simplifiedlinkedin.addresses.domain.mapper.ContinentMapper;
 import com.ahmedelzubair.simplifiedlinkedin.addresses.repo.ContinentJpaRepo;
+import com.ahmedelzubair.simplifiedlinkedin.exception.EntityNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class ContinentServiceImpl implements ContinentService {
 
     private final ContinentJpaRepo continentJpaRepo;
@@ -32,21 +35,54 @@ public class ContinentServiceImpl implements ContinentService {
 
     @Override
     public ContinentDTO updateContinent(ContinentDTO continentDTO) {
-        return null;
+
+        Continent continent = continentMapper.mapToContinentEntity(continentDTO);
+
+        Optional<Continent> continentOptional = continentJpaRepo.findById(continent.getId());
+
+        if (continentOptional.isEmpty()) {
+            throw new EntityNotFoundException("Continent with id " + continent.getId()
+                    + " does not exist");
+        }
+
+        Continent updatedContinent = continentJpaRepo.save(continent);
+
+        return continentMapper.mapToContinentDTO(updatedContinent);
     }
 
     @Override
     public void deleteContinent(ContinentDTO continentDTO) {
 
+        Continent continent = continentMapper.mapToContinentEntity(continentDTO);
+
+        Optional<Continent> continentOptional = continentJpaRepo.findById(continent.getId());
+
+        if (continentOptional.isEmpty()) {
+            throw new EntityNotFoundException("Continent with id " + continent.getId()
+                    + " does not exist");
+        }
+
+        continentJpaRepo.delete(continent);
+
     }
 
     @Override
     public Optional<ContinentDTO> findContinentById(Long continentId) {
-        return Optional.empty();
+        Optional<Continent> continentOptional = continentJpaRepo.findById(continentId);
+        if (continentOptional.isEmpty()) {
+            throw new EntityNotFoundException("Continent with id " + continentId
+                    + " does not exist");
+        }
+        return Optional.of(continentMapper.mapToContinentDTO(continentOptional.get()));
     }
 
     @Override
     public Optional<ContinentDTO> findContinentByName(String continentName) {
-        return Optional.empty();
+        Optional<Continent> continentOptional = continentJpaRepo.findByName(continentName);
+        if (continentOptional.isEmpty()) {
+            throw new EntityNotFoundException("Continent with name " + continentName
+                    + " does not exist");
+        }
+        return Optional.of(continentMapper.mapToContinentDTO(continentOptional.get()));
     }
 }
